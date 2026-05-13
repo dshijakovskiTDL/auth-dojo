9003a21488e9579a00d2e9a947ed4ce399e1a8a4
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2026-05-13 07:45:57 +0200

    Fix env handling on frontend

--- auth-client/vite.config.ts
+++ auth-client/vite.config.ts
@@ -5,10 +5,4 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    port: 5173,
-    watch:
-      process.env.DOCKER === 'true' ? { usePolling: true, interval: 300 } : undefined,
-  },
 });
